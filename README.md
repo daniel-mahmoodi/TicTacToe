1143ff011b904b1981de85fcca30fa61e409add5
daniel-mahmoodi <mahmoodidanial2@gmail.com>
2022-01-28 05:13:04 +0330

    addreact

--- README.md
+++
@@ -1,2 +0,0 @@
-# resumeSite_SDCoders_
-#first start in jan 26 2022
---
+++ sdcoders/src/components/Board.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Square from './Square';
+class Board extends Component {
+    state = {  } 
+    renderSquare(i) { 
+        return (<Square value={i}/>);
+    }
+    render(){
+        const status ="next Player: x"
+        return(
+            <div>
+            <div className='status'>
+                {status}
+            </div>
+            <div className='border-row'>
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className='border-row'>
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className='border-row'>
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+            </div>
+        );
+    }
+}
+ 
+export default Board;
